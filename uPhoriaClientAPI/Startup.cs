12f0b74679073f4271960d79f7abcefc0acbea55
Ken Nwankwo <ken_nwankwo@shi.com>
2022-03-28 14:03:11 -0400

    Added Nuget packages Added Swagger to Startup and commented out HTTPS redirection Added Models Folder and created classes Post and User nested within Changed LaunchURL in LaunchSettings of "profiles" and  "uPhoriaClientAPI" to empty and reflected the code in startup(c.RoutePrefix = string.Empty;)

--- uPhoriaClientAPI/Startup.cs
+++ uPhoriaClientAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,17 +27,28 @@ namespace uPhoriaClientAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "uPhoriaClientAPI", Version = "v1", Description = "Api for uPhoria" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "uPhoriaClientAPI v1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
