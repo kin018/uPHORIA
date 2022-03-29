d47a1221cdf66d9a090dd4384fff4cea5c4fb660
Ken Nwankwo <ken_nwankwo@shi.com>
2022-03-29 10:52:33 -0400

    Added properties to User model

--- uPhoriaClientAPI/Models/User.cs
+++ uPhoriaClientAPI/Models/User.cs
@@ -1,6 +1,27 @@
-﻿namespace uPhoriaClientAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace uPhoriaClientAPI.Models
 {
     public class User
     {
+        [Key]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public byte[] PasswordHash { get; set; }
+
+        [Required]
+        public byte[] PasswordSalt { get; set; }
+
     }
 }
