47f970c0e0dbd7a4bc927d39ea3b21a3bd46e542
Ken Nwankwo <ken_nwankwo@shi.com>
2022-03-30 16:07:11 -0400

    Upated PostService

--- uPhoriaClientAPI/Services/PostService.cs
+++ uPhoriaClientAPI/Services/PostService.cs
@@ -1,6 +1,37 @@
-﻿namespace uPhoriaClientAPI.Services
+﻿using System.Collections.Generic;
+using uPhoriaClientAPI.Data;
+using uPhoriaClientAPI.Interfaces;
+using uPhoriaClientAPI.Models;
+
+namespace uPhoriaClientAPI.Services
 {
-    public class PostService
+    public class PostService : IPostService
     {
+        private readonly DataContext _context;
+
+        public PostService(DataContext context)
+        {
+            _context = context;
+
+        }
+        public void CreatePost(Post post)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAllPosts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Post GetPostByID(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Savechanges()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
