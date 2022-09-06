4f21b6eed312ee8b8ddb41ac402bf2c988019b9f
Ken Nwankwo <ken_nwankwo@shi.com>
2022-09-06 10:31:40 -0400

    Formatting and Commenting

--- uPhoriaClientAPI/Startup.cs
+++ uPhoriaClientAPI/Startup.cs
@@ -27,7 +27,7 @@ namespace uPhoriaClientAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        //This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(opt =>
@@ -42,7 +42,7 @@ namespace uPhoriaClientAPI
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwagger();
